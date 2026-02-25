537ddea293bb18baec92701f37021ed82ccf81c7
auyyangon <info@connect.edu.mm>
2026-02-26 05:24:45 +0630

    Add missing Exams page

---
+++ src/pages/Exams.tsx
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import { GlassCard } from '../components/Common';
+
+export const Exams: React.FC = () => {
+  return (
+    <div className="space-y-6">
+      <h1 className="text-3xl font-medium text-gray-700">My Exams</h1>
+      <GlassCard>
+        <p className="text-gray-600">Exam timetable will appear here.</p>
+      </GlassCard>
+    </div>
+  );
+};

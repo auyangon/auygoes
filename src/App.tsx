c2a4514af6cf5fa5d0b52cbf1bd0ab65abb01138
auyyangon <info@connect.edu.mm>
2026-02-28 18:33:32 +0630

    Fix build error and add clickable announcements page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
 ﻿import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { DataProvider } from './contexts/DataContext';
-import { ExamProvider } from './contexts/exam/ExamContext';
 import Dashboard from './pages/Dashboard';
 import { Login } from './pages/Login';
-import { Exams } from './pages/Exams';
-import { AdminExams } from './pages/AdminExams';
+import { PublicQExams } from './pages/PublicQExams';
+import { AnnouncementsPage } from './pages/AnnouncementsPage';
 
 function App() {
   return (
     <AuthProvider>
       <DataProvider>
-        <ExamProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/exams" element={<Exams />} />
-              <Route path="/admin/exams" element={<AdminExams />} />
-              <Route path="/" element={<Navigate to="/dashboard" />} />
-            </Routes>
-          </BrowserRouter>
-        </ExamProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/exams" element={<PublicQExams />} />
+            <Route path="/announcements" element={<AnnouncementsPage />} />
+            <Route path="/" element={<Navigate to="/dashboard" />} />
+          </Routes>
+        </BrowserRouter>
       </DataProvider>
     </AuthProvider>
   );
